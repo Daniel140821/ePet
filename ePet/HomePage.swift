cc22d57faa2f57c40163625c697a3d4641eec04a
Daniel140821 <138580504+Daniel140821@users.noreply.github.com>
2025-07-31 10:28:56 +0800

    1.0

--- ePet/HomePage.swift
+++ ePet/HomePage.swift
@@ -11,7 +11,7 @@ struct HomePage: View {
     let player = SoundPlayer()
 
     @AppStorage("UserSelectedPet") var UserSelectedPet : String?
-    //var UserSelectedPet : String? = "tutu"
+    //var UserSelectedPet : String? = "Cat"
     @AppStorage("Money") var Money : Int = 0
     
     @AppStorage("HaveChicken") var HaveChicken : Int = 0
@@ -43,30 +43,40 @@ struct HomePage: View {
             HStack{
                 ScrollView(.horizontal){
                     HStack{
-                        if HaveChicken > 0{
-                            ZStack{}
-                                .padding(.trailing)
-                            ForEach(0..<HaveChicken){_ in
-                                Image("ChickenNoBG")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 80)
-                                    .onTapGesture{
-                                        HaveChicken = HaveChicken - 1
-                                        
-                                        player.playSound(soundName: "eat", type: "mp3")
-
-                                        
-                                        happy = true
-                                        
-                                        timer = Timer.scheduledTimer(withTimeInterval: 10,repeats: false) { [self] _ in
-                                            withAnimation{
-                                                happy = false
-                                            }
-                                            timer?.invalidate()
-                                        }
-                                        RunLoop.current.add(timer!, forMode: .common)
+                        ZStack{
+                            if HaveChicken > 0{
+                                VStack{
+                                    Spacer()
+                                    HStack{
+                                        Spacer()
+                                        Text("×\(HaveChicken)")
+                                            .padding(.vertical)
+                                            .padding(.horizontal,10)
                                     }
+                                }
+                                ForEach(0..<HaveChicken){_ in
+                                    Image("ChickenNoBG")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 80)
+                                        .padding(.horizontal)
+                                        .onTapGesture{
+                                            HaveChicken = HaveChicken - 1
+                                            
+                                            player.playSound(soundName: "eat", type: "m4a")
+                                            
+                                            
+                                            happy = true
+                                            
+                                            timer = Timer.scheduledTimer(withTimeInterval: 10,repeats: false) { [self] _ in
+                                                withAnimation{
+                                                    happy = false
+                                                }
+                                                timer?.invalidate()
+                                            }
+                                            RunLoop.current.add(timer!, forMode: .common)
+                                        }
+                                }
                             }
                         }
                     }
@@ -91,9 +101,9 @@ struct HomePage: View {
                     timer?.invalidate()
                     
                     if UserSelectedPet == "Cat"{
-                        let num = Int.random(in: 1...3)
+                        let num = Int.random(in: 1...2)
                         
-                        player.playSound(soundName: "cat\(num)", type: "mp3")
+                        player.playSound(soundName: "cat\(num)", type: "m4a")
                         
                         print(num)
                         
@@ -109,7 +119,9 @@ struct HomePage: View {
                         }
                         RunLoop.current.add(timer!, forMode: .common)
                     }else{
-                        player.playSound(soundName: "tutu1", type: "wav")
+                        let num = Int.random(in: 1...2)
+                        
+                        player.playSound(soundName: "tutu\(num)", type: "m4a")
                         
                         withAnimation{
                             happy = true
